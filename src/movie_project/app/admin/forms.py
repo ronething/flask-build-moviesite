1754381e5cc6ec0980d9d9e202343f5686ef7db6
ronething <13414113153@163.com>
2018-10-23 22:18:47 +0800

    实现
    电影添加 编辑 删除
    
    文件上传功能

--- src/movie_project/app/admin/forms.py
+++ src/movie_project/app/admin/forms.py
@@ -9,10 +9,15 @@ Less is more.
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, \
+    SubmitField, FileField, TextAreaField, \
+    SelectField
 from wtforms.validators import DataRequired, ValidationError
 
-from app.models import Admin
+from app.models import Admin, Tag
+
+# 查询所有标签
+tags = Tag.query.all()
 
 
 class LoginForm(FlaskForm):
@@ -28,7 +33,7 @@ class LoginForm(FlaskForm):
         render_kw={
             "class": "form-control",
             "placeholder": "请输入账号！",
-            "required": "required"
+            "required": False
         }
     )
 
@@ -41,7 +46,7 @@ class LoginForm(FlaskForm):
         render_kw={
             "class": "form-control",
             "placeholder": "请输入密码！",
-            "required": "required"
+            "required": False
         }
     )
 
@@ -72,7 +77,8 @@ class TagForm(FlaskForm):
         render_kw={
             "class": "form-control",
             "id": "input_name",
-            "placeholder": "请输入标签名称！"
+            "placeholder": "请输入标签名称！",
+            "required": False
         }
     )
 
@@ -82,3 +88,140 @@ class TagForm(FlaskForm):
             "class": "btn btn-primary"
         }
     )
+
+
+class MovieForm(FlaskForm):
+    """
+    电影表单
+    """
+    title = StringField(
+        label="片名",
+        validators=[
+            DataRequired("请输入片名！")
+        ],
+        description="片名",
+        render_kw={
+            "class": "form-control",
+            "id": "input_title",
+            "placeholder": "请输入片名！",
+            "required": False
+        }
+    )
+
+    url = FileField(
+        label="文件",
+        validators=[
+            DataRequired("请上传文件")
+        ],
+        description="文件",
+        render_kw={
+            "id": "input_url",
+            "required": False
+        }
+    )
+
+    info = TextAreaField(
+        label="简介",
+        validators=[
+            DataRequired("请输入简介！")
+        ],
+        description="简介",
+        render_kw={
+            "class": "form-control",
+            "rows": "10",
+            "id": "input_info",
+            "required": False
+        }
+    )
+
+    logo = FileField(
+        label="封面",
+        validators=[
+            DataRequired("请上传封面")
+        ],
+        description="封面",
+        render_kw={
+            "id": "input_logo",
+            "required": False
+        }
+    )
+
+    star = SelectField(
+        label="星级",
+        validators=[
+            DataRequired("请选择星级！")
+        ],
+        coerce=int,
+        choices=[(1, "1星"), (2, "2星"), (3, "3星"), (4, "4星"), (5, "5星")],
+        description="星级",
+        render_kw={
+            "class": "form-control",
+            "id": "input_star",
+            "required": False
+        }
+    )
+
+    tag_id = SelectField(
+        label="标签",
+        validators=[
+            DataRequired("请选择标签！")
+        ],
+        coerce=int,
+        choices=[(i.id, i.name) for i in tags],
+        description="标签",
+        render_kw={
+            "class": "form-control",
+            "id": "input_tag_id",
+            "required": False
+        }
+    )
+
+    area = StringField(
+        label="地区",
+        validators=[
+            DataRequired("请输入地区！")
+        ],
+        description="地区",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入地区！",
+            "id": "input_area",
+            "required": False
+        }
+    )
+
+    length = StringField(
+        label="片长",
+        validators=[
+            DataRequired("请输入片长！")
+        ],
+        description="片长",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入片长！",
+            "id": "input_length",
+            "required": False
+        }
+    )
+
+    release_time = StringField(
+        label="上映时间",
+        validators=[
+            DataRequired("请选择上映时间！")
+        ],
+        description="上映时间",
+        render_kw={
+            "class": "form-control",
+            "id": "input_release_time",
+            "placeholder": "请选择上映时间！",
+            "required": False
+        }
+    )
+
+    submit = SubmitField(
+        "确认",
+        render_kw={
+            "class": "btn btn-primary"
+        }
+    )
+
--- src/movie_project/app/admin/views.py
+++ src/movie_project/app/admin/views.py
@@ -8,12 +8,18 @@
 Less is more.
 """
 
+import os
+import uuid
+import datetime
+
 from . import admin
 from flask import render_template, url_for, redirect, flash, session, request
-from app.admin.forms import LoginForm, TagForm
-from app.models import Admin, Tag
+from app.admin.forms import LoginForm, TagForm, MovieForm
+from app.models import Admin, Tag, Movie
 from functools import wraps
-from app import db
+from app import db,app
+from werkzeug.utils import secure_filename
+from pypinyin import lazy_pinyin
 
 
 # 登陆控制装饰器
@@ -25,6 +31,16 @@ def admin_login_req(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+
+# 修改文件名称
+def rename_filename(filename):
+    fileinfo = os.path.splitext(filename)
+    filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S") \
+               + str(uuid.uuid4().hex) \
+               + fileinfo[-1]
+
+    return filename
 
 
 @admin.route("/")
@@ -70,14 +86,14 @@ def tag_add():
         tag = Tag.query.filter_by(name=data["name"]).count()
         if tag == 1:
             flash("该标签已经存在", "err")
-            return redirect(url_for("admin.tag_add"))
+            return render_template("admin/tag_add.html", form=form)
         tag = Tag(
             name=data["name"]
         )
         db.session.add(tag)
         db.session.commit()
         flash("添加标签成功", "ok")
-        redirect(url_for("admin.tag_add"))
+        return redirect(url_for("admin.tag_add"))
     return render_template("admin/tag_add.html", form=form)
 
 
@@ -103,16 +119,16 @@ def tag_edit(id=None):
     tag = Tag.query.filter_by(id=id).first_or_404()
     if form.validate_on_submit():
         data = form.data
-        tagcount = Tag.query.filter_by(name=data["name"]).count()
+        tag_count = Tag.query.filter_by(name=data["name"]).count()
         # 例如有标签 1 2 现在把 2 改为 1 则要抛出异常 如果 2 不改还是 2 则可以更改成功
-        if tag.name != data["name"] and tagcount == 1:
+        if tag.name != data["name"] and tag_count == 1:
             flash("该标签已经存在", "err")
             return redirect(url_for("admin.tag_edit", id=id))
         tag.name = data["name"]
         db.session.add(tag)
         db.session.commit()
         flash("修改标签成功", "ok")
-        redirect(url_for("admin.tag_edit", id=id))
+        return redirect(url_for("admin.tag_edit", id=id))
     return render_template("admin/tag_edit.html", form=form, tag=tag)
 
 
@@ -127,17 +143,136 @@ def tag_del(id=None):
     return redirect(url_for("admin.tag_list"))
 
 
-
-@admin.route("/movie/add/")
+# 电影添加
+@admin.route("/movie/add/", methods=["GET", "POST"])
 @admin_login_req
 def movie_add():
-    return render_template("admin/movie_add.html")
+    form = MovieForm()
+    if form.validate_on_submit():
+        data = form.data
+        # 判断是否标题重复
+        movie = Movie.query.filter_by(title=data["title"]).count()
+        if movie == 1:
+            flash("该电影已经存在", "err")
+            return render_template("admin/movie_add.html", form=form)
+
+        # 先用 pypinyin 将中文转为拼音 这样 secure_filename 就不会因为文件名是中文而过滤掉了
+        # 参考 https://pdf-lib.org/Home/Details/6002
+        # 保存封面和短片文件
+        if not os.path.exists(app.config["UPLOAD_DIR"]):
+            os.makedirs(app.config["UPLOAD_DIR"])
+            os.chmod(app.config["UPLOAD_DIR"], "rw")
+
+        file_url = secure_filename("".join(lazy_pinyin(form.url.data.filename)))
+        url = rename_filename(file_url)
+        form.url.data.save(app.config["UPLOAD_DIR"] + url)
+
+        file_logo = secure_filename("".join(lazy_pinyin(form.logo.data.filename)))
+        logo = rename_filename(file_logo)
+        form.logo.data.save(app.config["UPLOAD_DIR"] + logo)
+
+        movie = Movie(
+            title=data["title"],
+            url=url,
+            info=data["info"],
+            logo=logo,
+            star=int(data["star"]),
+            playnum=0,
+            commentnum=0,
+            tag_id=int(data["tag_id"]),
+            area=data["area"],
+            release_time=data["release_time"],
+            length=data["length"],
+        )
+        db.session.add(movie)
+        db.session.commit()
+        flash("添加电影成功", "ok")
+        return redirect(url_for("admin.movie_add"))
+    return render_template("admin/movie_add.html", form=form)
 
 
-@admin.route("/movie/list/")
+# 电影列表
+@admin.route("/movie/list/", methods=["GET"])
+@admin.route("/movie/list/<int:page>/", methods=["GET"])
 @admin_login_req
-def movie_list():
-    return render_template("admin/movie_list.html")
+def movie_list(page=None):
+    # 默认为第一页
+    if page is None:
+        page = 1
+    page_data = Movie.query.join(Tag).filter(
+      Movie.tag_id == Tag.id
+    ).order_by(
+        Movie.addtime.asc()
+    ).paginate(page=page, per_page=2)
+    return render_template("admin/movie_list.html", page_data=page_data)
+
+
+# 电影删除
+@admin.route("/movie/del/<int:id>/", methods=["GET"])
+@admin_login_req
+def movie_del(id=None):
+    movie = Movie.query.filter_by(id=id).first_or_404()
+    db.session.delete(movie)
+    db.session.commit()
+    flash("删除电影成功", "ok")
+    return redirect(url_for("admin.movie_list"))
+
+
+# 电影编辑
+@admin.route("/movie/edit/<int:id>/", methods=["GET", "POST"])
+@admin_login_req
+def movie_edit(id=None):
+    form = MovieForm()
+    form.url.validators = []
+    form.logo.validators = []
+    movie = Movie.query.filter_by(id=id).first_or_404()
+
+    if request.method == "GET":
+        form.info.data = movie.info
+        form.star.data = movie.star
+        form.tag_id.data = movie.tag_id
+
+    if form.validate_on_submit():
+        data = form.data
+        movie_count = Movie.query.filter_by(title=data["title"]).count()
+        if movie.title != data["title"] and movie_count == 1:
+            flash("该电影名已经存在", "err")
+            return redirect(url_for("admin.movie_edit", id=id))
+
+        # 先用 pypinyin 将中文转为拼音 这样 secure_filename 就不会因为文件名是中文而过滤掉了
+        # 参考 https://pdf-lib.org/Home/Details/6002
+        # 保存封面和短片文件
+        if not os.path.exists(app.config["UPLOAD_DIR"]):
+            os.makedirs(app.config["UPLOAD_DIR"])
+            os.chmod(app.config["UPLOAD_DIR"], "rw")
+
+        # 如果表单 url 数据不为空 说明上传了文件
+        # 如果表单的 url 为空 则 form.url.data 为空 且为 str 类型
+        if form.url.data:
+            file_url = secure_filename("".join(lazy_pinyin(form.url.data.filename)))
+            movie.url = rename_filename(file_url)
+            form.url.data.save(app.config["UPLOAD_DIR"] + movie.url)
+
+        # 如果表单 logo 数据不为空 说明上传了文件
+        # 如果表单的 logo 为空 则 form.logo.data 为空 且为 str 类型
+        if form.logo.data:
+            file_logo = secure_filename("".join(lazy_pinyin(form.logo.data.filename)))
+            movie.logo = rename_filename(file_logo)
+            form.logo.data.save(app.config["UPLOAD_DIR"] + movie.logo)
+
+        movie.title = data["title"]
+        movie.info = data["info"]
+        movie.star = data["star"]
+        movie.tag_id = data["tag_id"]
+        movie.area = data["area"]
+        movie.length = data["length"]
+        movie.release_time = data["release_time"]
+
+        db.session.add(movie)
+        db.session.commit()
+        flash("修改电影成功", "ok")
+        return redirect(url_for("admin.movie_edit", id=id))
+    return render_template("admin/movie_edit.html", form=form, movie=movie)
 
 
 @admin.route("/preview/add/")
