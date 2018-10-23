bbe2496fda69e7d7b2d992887dc9e66f6417ddab
ronething <13414113153@163.com>
2018-10-24 02:04:35 +0800

    上映预告 添加 删除 编辑 功能

--- src/movie_project/app/admin/views.py
+++ src/movie_project/app/admin/views.py
@@ -14,8 +14,8 @@ import datetime
 
 from . import admin
 from flask import render_template, url_for, redirect, flash, session, request
-from app.admin.forms import LoginForm, TagForm, MovieForm
-from app.models import Admin, Tag, Movie
+from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
+from app.models import Admin, Tag, Movie, Preview
 from functools import wraps
 from app import db,app
 from werkzeug.utils import secure_filename
@@ -275,16 +275,94 @@ def movie_edit(id=None):
     return render_template("admin/movie_edit.html", form=form, movie=movie)
 
 
-@admin.route("/preview/add/")
+# 上映预告添加
+@admin.route("/preview/add/", methods=["GET", "POST"])
 @admin_login_req
 def preview_add():
-    return render_template("admin/preview_add.html")
+    form = PreviewForm()
+    if form.validate_on_submit():
+        data = form.data
+        preview = Preview.query.filter_by(title=data["title"]).count()
+        if preview == 1:
+            flash("该上映预告已经存在", "err")
+            return render_template("admin/preview_add.html", form=form)
+
+        if not os.path.exists(app.config["UPLOAD_DIR"]):
+            os.makedirs(app.config["UPLOAD_DIR"])
+            os.chmod(app.config["UPLOAD_DIR"], "rw")
+
+        file_logo = secure_filename("".join(lazy_pinyin(form.logo.data.filename)))
+        logo = rename_filename(file_logo)
+        form.logo.data.save(app.config["UPLOAD_DIR"] + logo)
+
+        preview = Preview(
+            title=data["title"],
+            logo=logo,
+        )
+        db.session.add(preview)
+        db.session.commit()
+        flash("添加上映预告成功", "ok")
+        return redirect(url_for("admin.preview_add"))
+    return render_template("admin/preview_add.html", form=form)
 
 
-@admin.route("/preview/list/")
+# 上映预告列表
+@admin.route("/preview/list/", methods=["GET"])
+@admin.route("/preview/list/<int:page>/", methods=["GET"])
 @admin_login_req
-def preview_list():
-    return render_template("admin/preview_list.html")
+def preview_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Preview.query.order_by(
+        Preview.addtime.asc()
+    ).paginate(page=page, per_page=5)
+    return render_template("admin/preview_list.html", page_data=page_data)
+
+
+# 上映预告删除
+@admin.route("/preview/del/<int:id>/", methods=["GET"])
+@admin_login_req
+def preview_del(id=None):
+    preview = Preview.query.filter_by(id=id).first_or_404()
+    db.session.delete(preview)
+    db.session.commit()
+    flash("删除电影成功", "ok")
+    return redirect(url_for("admin.preview_list"))
+
+
+# 上映预告编辑
+@admin.route("/preview/edit/<int:id>/", methods=["GET", "POST"])
+@admin_login_req
+def preview_edit(id=None):
+    form = PreviewForm()
+    form.logo.validators = []
+    preview = Preview.query.filter_by(id=id).first_or_404()
+
+    if form.validate_on_submit():
+        data = form.data
+        preview_count = Preview.query.filter_by(title=data["title"]).count()
+        if preview.title != data["title"] and preview_count == 1:
+            flash("该上映预告已经存在", "err")
+            return redirect(url_for("admin.preview_edit", id=id))
+
+        if not os.path.exists(app.config["UPLOAD_DIR"]):
+            os.makedirs(app.config["UPLOAD_DIR"])
+            os.chmod(app.config["UPLOAD_DIR"], "rw")
+
+        # 如果表单 logo 数据不为空 说明上传了文件
+        # 如果表单的 logo 为空 则 form.logo.data 为空 且为 str 类型
+        if form.logo.data:
+            file_logo = secure_filename("".join(lazy_pinyin(form.logo.data.filename)))
+            preview.logo = rename_filename(file_logo)
+            form.logo.data.save(app.config["UPLOAD_DIR"] + preview.logo)
+
+        preview.title = data["title"]
+
+        db.session.add(preview)
+        db.session.commit()
+        flash("修改上映预告成功", "ok")
+        return redirect(url_for("admin.preview_edit", id=id))
+    return render_template("admin/preview_edit.html", form=form, preview=preview)
 
 
 @admin.route("/user/list/")
